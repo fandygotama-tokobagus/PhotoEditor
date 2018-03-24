25397fdd097480fc231b4612a6e630492daf4544
Fandy Gotama <fandy.gotama@olx.co.id>
2018-03-24 21:38:14 +0700

    Update constant

--- photoeditor/src/main/java/ja/burhanrashid52/views/Constants.java
+++ photoeditor/src/main/java/ja/burhanrashid52/views/Constants.java
@@ -4,5 +4,5 @@ package ja.burhanrashid52.views;
  * Created by panyi on 2016/8/6.
  */
 public class Constants {
-    public static final int STICKER_BTN_HALF_SIZE = 30;
+    public static final int STICKER_BTN_HALF_SIZE = 25;
 }
